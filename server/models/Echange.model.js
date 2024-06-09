a5b2866f364caa52507c8fe7e4fa303a0e456672
soukimahdi <mahdi.souki@nextav.ai>
2024-06-09 23:07:41 +0100

    echange

--- server/models/Echange.model.js
+++ server/models/Echange.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const EchangeSchema = new mongoose.Schema({
   nom: { type: String, required: true },
-  description: { type: String, required: true },
-  prix: { type: Float32Array, required: true },
-  quatityDispo: { type: String, required: true },
+  firstUser: { type: mongoose.Types.ObjectId, ref: "user" },
+  seconddUser: { type: mongoose.Types.ObjectId, ref: "user" },
+  products: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
 });
 
 module.exports = mongoose.model("Echange", EchangeSchema);
